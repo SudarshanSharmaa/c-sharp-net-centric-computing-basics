06ba4529a29b8765bee09306da9dff9da7b2843c
SudarshanSharmaa <sudarshansharma72@gmail.com>
2026-01-20 14:47:58 +0545

    basic c#

---
+++ input and output/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input_and_output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int harry = 34;
+            string inp = "Harry";
+            Console.WriteLine(inp);
+            // Console.WriteLine("Hello World");
+            //Console.Write("Hello Ram");
+            //Console.WriteLine("I love c# and the number" + harry);
+            int a = 10;
+            float b = 34.4F;
+            double c = 37.7;
+            bool isGreat = true;
+            char d = 'a';
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(isGreat);
+            Console.WriteLine(d);
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ input and output/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("input and output")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("input and output")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6dcdf2ed-b29c-4d97-b24e-d57128cdda78")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
